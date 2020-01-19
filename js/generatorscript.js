e749d1f9c23266cc03bb6bfa2ae087a0416c5987
PaulKania <p.rozehnal@gmail.com>
2020-01-18 20:33:07 -0500

    remove console-log

--- js/generatorscript.js
+++ js/generatorscript.js
@@ -15,20 +15,15 @@ for (let i = 0; i < morsels_strings.length; i++) {
 var mousecount = 0
 var changeoccurs = false
 function ChangeMorsel() {
-  console.log('top');
   while (changeoccurs == false){
-    console.log('inwhile');
     if (mousecount<morsels.length){
-      console.log(mousecount,'infirstif');
       var randomchoice = Math.floor(Math.random()*Math.floor(morsels.length))
       if (morsels[randomchoice].visited == true){
-        console.log('in2ndif');
         continue;  }
     morsels[randomchoice].visited = true
     changeoccurs = true
     mousecount += 1
     document.getElementById("ephemeral_line").innerHTML = morsels[randomchoice].string
-    console.log(mousecount,changeoccurs);
     }
     }
 }
